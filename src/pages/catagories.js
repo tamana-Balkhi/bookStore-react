3961389c835dce50c35fc9e0bc026b3beef0adca
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-18 04:13:48 +0430

    add categories

---
+++ src/pages/catagories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <div>
+      <h1>Categories Bookstore</h1>
+      <button value="category" type="submit">ADD NEW CATEGORY</button>
+    </div>
+  );
+}
+
+export default Categories;
