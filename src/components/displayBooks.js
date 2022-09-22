5a3615bea08d3ba63d83e7ccd82077cd81d4d2d5
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-22 19:21:11 +0430

    functionality for "Remove" button click

--- src/components/displayBooks.js
+++ src/components/displayBooks.js
@@ -1,26 +1,27 @@
 /* eslint-disable react/prop-types,  react/destructuring-assignment */
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 function DisBooks(props) {
   const {
     book,
   } = props;
+  const dispatch = useDispatch();
   return (
     <div className="book">
       <div className="b-info">
         <h2>{ book.title }</h2>
         <h3>{ book.author }</h3>
         <div className="actions-container">
-          <button type="button" className="btn" id="comment">Comments</button>
-          <button type="button" className="btn" id="comment">Remove</button>
-          <button type="button" className="btn" id="comment">Edit</button>
+          <button type="button" className="btn" id="btn">Comments</button>
+          <button type="button" className="btn" id="btn" onClick={() => dispatch(removeBook(book))}>Remove</button>
+          <button type="button" className="btn" id="btn">Edit</button>
         </div>
       </div>
 
       <div className="progress-percent">
         <h3>
           { book.percentage }
-          {' '}
-          0%
         </h3>
       </div>
 
