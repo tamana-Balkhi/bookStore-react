2b1859707e4d2648af2f0b3ff6d84baa342dcb04
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-22 19:13:25 +0430

    change the default state in  books reducer from an empty array to an array with  few books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,21 +1,49 @@
 // Actions for books
-const ADD_BOOK = 'bookstore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const ADD_BOOK = 'books/ADD_BOOK';
+const REMOVE_BOOK = 'books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [{
+  id: 1,
+  title: 'the Hunger Games',
+  percentage: 64,
+  author: 'Suzanne Collins',
+  chapter: 17,
+},
+{
+  id: 2,
+  title: 'Dune',
+  percentage: 35,
+  author: 'Frank Herbert',
+  chapter: 3,
+},
+{
+  id: 3,
+  title: 'The Capital of Twenry-First century',
+  percentage: 18,
+  author: 'Suzanne Collins',
+  chapter: 'Introduction',
+}];
+
+// Actions creators
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (book) => ({
+  type: REMOVE_BOOK,
+  book,
+});
 
 // Reducer
-export default function booksReducer(state = initialState, action) {
+function booksReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.payload];
+      return state.concat(action.book);
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.book.id);
     default: return state;
   }
 }
 
-// Actions creators
-export const addBook = (book) => ({ type: ADD_BOOK, book });
-
-export const removeBook = (book) => ({ type: REMOVE_BOOK, book });
+export default booksReducer;
