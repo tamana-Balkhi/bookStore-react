c111a9f7b1e97688198b6a3e7cf611f8a5c95c07
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-24 17:02:12 +0430

    use Api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,13 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8jPJscW2sY5rPxOZDiOs/books';
+const appID = 'z4gzcApO94uwV3qBSQiu';
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+const apiURL = `${baseUrl + appID}/books`;
 
 const initialState = [];
 
-const addBook = createAsyncThunk('books/addBook', async (book) => {
+export const addBook = createAsyncThunk('books/addBook', async (book) => {
   const response = await axios.post(apiURL, book);
   if (response.status === 201) {
     return book;
@@ -13,17 +15,17 @@ const addBook = createAsyncThunk('books/addBook', async (book) => {
   return response.data;
 });
 
-const getBooks = createAsyncThunk('books/getBooks', async () => {
+export const getBooks = createAsyncThunk('books/getBooks', async () => {
   const response = await axios.get(apiURL);
   return response.data;
 });
 
-const removeBook = createAsyncThunk('books/removeBook', async (book) => {
+export const removeBook = createAsyncThunk('books/removeBook', async (book) => {
   await axios.delete(`${apiURL}/${book.item_id}`);
   return book;
 });
 
-const booksSlice = createSlice({
+export const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {},
@@ -34,25 +36,17 @@ const booksSlice = createSlice({
         // Add book
         state.push(action.payload);
       })
-      .addCase(addBook.pending, (state, action) => {
-        // Add book
-        state.push(action.payloadCreator);
-      })
       .addCase(addBook.rejected, (state, action) => {
         // Add book
-        state.push(action.error.message);
+        state.push(action.error.messege);
       })
-    // getbook
+
       .addCase(getBooks.fulfilled, (state, action) => Object.entries(action.payload).map(
-        ([id, [book]]) => ({ ...book, item_id: id }),
-      ))
-      .addCase(getBooks.pending, (state, action) => Object.entries(action.payloadCreator).map(
         ([id, [book]]) => ({ ...book, item_id: id }),
       ))
       .addCase(getBooks.rejected, (state, action) => Object.entries(action.error.message).map(
         ([id, [book]]) => ({ ...book, item_id: id }),
       ))
-      // remove
       .addCase(removeBook.fulfilled, (state, action) => {
         state.forEach((book) => {
           if (book.item_id === action.payload.item_id) {
@@ -64,4 +58,3 @@ const booksSlice = createSlice({
 });
 
 export default booksSlice.reducer;
-export { getBooks, removeBook, addBook };
