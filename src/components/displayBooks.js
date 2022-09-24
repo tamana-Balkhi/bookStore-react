ecaaa604d1532d6aee2bfe740f39e37b031e8b90
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-25 02:07:40 +0430

    add circlebar

--- src/components/displayBooks.js
+++ src/components/displayBooks.js
@@ -1,38 +1,86 @@
 /* eslint-disable react/prop-types,  react/destructuring-assignment */
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import { removeBook } from '../redux/books/books';
 
+function CircularProgressWithLabel(props) {
+  /* eslint-disable */
+  return <CircularProgress variant="determinate" {...props} size={"80px"} />;
+  /* eslint-enable */
+}
+
+export function CircularStatic() {
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    setProgress(30 + Math.floor(Math.random() * 10));
+  }, []);
+
+  return <CircularProgressWithLabel value={progress} />;
+}
+
 function DisBooks(props) {
-  const {
-    book,
-  } = props;
+  const { book } = props;
   const dispatch = useDispatch();
   return (
-    <div className="book">
+    <div className="book" id={book.item_id}>
       <div className="b-info">
-        <h2>{ book.title }</h2>
-        <h3>{ book.author }</h3>
+        <span className="b-category">{book.category}</span>
+        <h2>{book.title}</h2>
+        <h3 className="b-author">{book.author}</h3>
         <div className="actions-container">
-          <button type="button" className="btn" id="btn">Comments</button>
-          <button type="button" className="btn" id="btn" onClick={() => dispatch(removeBook(book))}>Remove</button>
-          <button type="button" className="btn" id="btn">Edit</button>
+          <button type="button" className="btn comment">
+            Comments
+          </button>
+          <button
+            type="button"
+            className="btn remove-btn"
+            id="comment"
+            onClick={() => dispatch(removeBook(book))}
+          >
+            Remove
+          </button>
+          <button type="button" className="btn" id="comment">
+            Edit
+          </button>
         </div>
       </div>
 
       <div className="progress-percent">
-        <h3>
-          { book.percentage }
-        </h3>
+        <CircularStatic />
+        <div className="percent-graph">
+          <span className="percentage">
+            {Math.floor(Math.random() * 101)}
+            %
+          </span>
+          <span>Completed</span>
+        </div>
       </div>
 
       <div className="update-progress">
         <span>Current Chapter</span>
-        <h2>{ book.chapter }</h2>
-        <button type="button" className="update">Update progress</button>
+        <h2>
+          Chapter
+          {' '}
+          {Math.floor(Math.random() * 101)}
+        </h2>
+        <button type="button">Update progress</button>
       </div>
-
     </div>
   );
 }
+
+DisBooks.propTypes = {
+  book: PropTypes.shape(
+    {
+      title: PropTypes.string,
+      author: PropTypes.string,
+      category: PropTypes.string,
+      item_id: PropTypes.string,
+    },
+  ).isRequired,
+};
 
 export default DisBooks;
